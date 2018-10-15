477af56aebe4067ea6861eae9c99a1ad1d3ddbdc
David H <davedavedave77@users.noreply.github.com>
2018-10-15 12:59:57 +0200

    first commit; MS written yet; Implement more data stuff

---
+++ ProductCatalogService/Models/ProductModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IEGProductCatalogService.Models
+{
+    public class ProductModel
+    {
+        /*ProductModel()
+        {
+        }*/
+
+        public ProductModel(int productId, string productPublisher, string productName, int productPrice)
+        {
+            productId = ProductId;
+            productPublisher = ProductPublisher;
+            productName = ProductName;
+            productPrice = ProductPrice;
+        }
+
+        public ProductModel()
+        {
+            ProductId = 01;
+            ProductPublisher = "Best Producer";
+            ProductName = "BestProduct";
+            ProductPrice = 1;
+        }
+
+        public int ProductId { get; set; }
+        [System.ComponentModel.DataAnnotations.MinLength(2)]
+        public string ProductPublisher { get; set; }
+        public string ProductName { get; set; }
+        public int ProductPrice { get; set; }
+
+        public static implicit operator List<object>(ProductModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
