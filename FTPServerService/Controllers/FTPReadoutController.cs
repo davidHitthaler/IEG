4913500e27cd9692d8ee60cb20d3fafbf38521fb
David H <david.hitthaler@edu.campus02.at>
2018-10-20 17:20:40 +0200

    work together...: start stuff for Task2b FTP...

---
+++ FTPServerService/Controllers/FTPReadoutController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IEGProductCatalogService.Models;
+
+namespace FTPServerService.Controllers
+{
+    public class FTPReadoutController
+    {
+        private List<ProductModel> _products = new List<ProductModel>();
+    }
+}
