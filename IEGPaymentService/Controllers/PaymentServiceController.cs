497acd61201cdaa6a0544f03e120f6a5ff27e449
akramisch@hotmail.com <akramisch@hotmail.com>
2018-11-08 21:30:50 +0100

    andere Variante, post geht nur csv post geht nicht

--- IEGPaymentService/Controllers/PaymentServiceController.cs
+++ IEGPaymentService/Controllers/PaymentServiceController.cs
@@ -12,27 +12,21 @@ namespace PaymentService.Controllers
     [Route("api/PaymentService")]
     public class PaymentServiceController : Controller
     {
-        PaymentStorage _paymentStorage = new PaymentStorage();
+        private PaymentStorage _paymentStorage = new PaymentStorage();
+
         // GET: api/PaymentService
         [HttpGet]
         public IEnumerable<PaymentModel> Get()
         {
-            return _paymentStorage.Get();
+            return _paymentStorage.Payments;
         }
-
-        [HttpGet("{id}")]
-        public PaymentModel Get(int id)
-        {
-            return _paymentStorage.Get(id);            
-        }
-
 
         [HttpPost]
-        public IActionResult Post([FromBody] PaymentModel paymentModel) //TODO Datatyp return value
-        { 
-            _paymentStorage.Add(paymentModel);
-            return CreatedAtAction("Payment Added, ID = ", new { id = paymentModel.PaymentId });
-            //return _paymentStorage.Get(); //IEnumerable<PaymentModel>
+        public IEnumerable<PaymentModel> Post([FromBody] PaymentModel payment)
+        {
+            _paymentStorage.Payments.Add(payment);
+            //return CreatedAtAction("Payment Added, ID = ", new { id = paymentModel.PaymentId });
+            return _paymentStorage.Payments; //IEnumerable<PaymentModel>
         }
     }
 }
