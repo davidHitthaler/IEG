cb1c72c736463c84c09cf5d74a0c25c4fe9ce977
Adam Dolgos <adam.dolgos@edu.campus02.at>
2018-11-09 13:49:25 +0100

    Webhook - erste Schritte

---
+++ GithubWebhookService/Controllers/WebhookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace GithubWebhookService.Controllers
+{
+    [Route("api/[controller]")]
+    public class WebhookController : Controller
+    {
+
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(string id, string @event, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+    }
+}
